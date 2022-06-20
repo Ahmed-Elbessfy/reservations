3ba4f30109bbbeb053b5ae4409caee7a556f0eb7
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2022-06-20 16:40:18 +0200

    fetch reservations in Reservations comp
    - import useState to store reservations in Reservations comp. state
    - initiate reserveList to store reservations
    - create fetchData function to fetch date from Data base
    - use useEffect to call fetchData and get Reservations data

--- src/components/reservations/Reservations.js
+++ src/components/reservations/Reservations.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import ReservationsList from './ReservationsList'
+import { useEffect, useState } from "react";
+import ReservationsList from "./ReservationsList";
 
 const Reservations = () => {
+  // ************* Component State *************** //
+  const [reserveList, setReserveList] = useState([]);
+  // ************* Component Methods *************** //
+  const fetchData = async () => {
+    const data = await fetch("./assets/data.json");
+    const result = await data.json();
+    setReserveList(result.reservations);
+  };
+
+  // fetching data
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <section>
       <ReservationsList />
     </section>
-  )
-}
+  );
+};
 
-export default Reservations
+export default Reservations;
